8e04813b6c5e80dd2463078ee0702ae9f7eeb288
Omkar563 <74735039+Omkar563@users.noreply.github.com>
2023-06-17 15:17:19 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -35,7 +35,7 @@ int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
     int tc = 1;
-    cin >> tc;
+    // cin >> tc;
     for (int t = 1; t <= tc; t++) {
         // cout << "Case #" << t << ": ";
         solve();
